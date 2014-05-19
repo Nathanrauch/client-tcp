1b999ab1a7944ab8d82ec95315940c07d59255f1
Nathan <nathan@wolfssl.com>
2014-05-19 15:28:41 -0600

    Updated stuff

--- Client-TCP/client-tcp.c
+++ Client-TCP/client-tcp.c
@@ -1,4 +1,5 @@
-/*client-tcp.c
+
+/* client-tcp.c
  *
  * Copyright (C) 2006-2014 wolfSSL Inc.
  *
@@ -19,36 +20,40 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
  * USA */
 
-#include	<sys/socket.h>	/* basic socket definitions */
-#include	<netinet/in.h>	/* sockaddr_in{} and other Internet defns */
-#include	<stdio.h>
-#include	<stdlib.h>
-#include	<string.h>
-#include	<errno.h>
-#include	<arpa/inet.h>
-#include	<signal.h>
+#include    <sys/socket.h>	/* basic socket definitions */
+#include    <netinet/in.h>	/* sockaddr_in{} and other Internet defns */
+#include    <stdio.h>
+#include    <stdlib.h>
+#include    <string.h>
+#include    <errno.h>
+#include    <arpa/inet.h>
+#include    <signal.h>
 #include    <unistd.h>
-#define     MAXLINE     4096    /* max text line length */
-#define     SERV_PORT   9877
-#define SA  struct sockaddr
 
-void 
-str_Client(FILE *fp, int sockfd){
-    char sendline[MAXLINE]; /*String to send to the server*/
-    char recvline[MAXLINE]; /*String received from the server*/
+#define    MAXLINE     256    /* max text line length */
+#define    SERV_PORT   11111
+#define    SA  struct sockaddr
 
-    while(fgets(sendline, MAXLINE, fp) != NULL){
+/*
+ * this function will send the inputted string to the server and then 
+ * recieve the string from the server outputing it to the termial
+ */ 
+void SendReceive(FILE *fp, int sockfd){
+    char sendline[MAXLINE]; /* string to send to the server */
+    char recvline[MAXLINE]; /* string received from the server */
+
+    while (fgets(sendline, MAXLINE, fp) != NULL) {
        
-        /*write string to the server*/
+        /* write string to the server */
         write(sockfd, sendline, strlen(sendline));
        
-        /*Flags if the Server stopped before the client could end*/     
+        /* flags if the server stopped before the client could end */     
         if(read(sockfd, recvline, MAXLINE) == 0){
             printf("Client: Server Terminated Prematurely!\n");
             exit(0);
         }
 
-        /*Writes the string supplied to the indicated output stream*/
+        /* writes the string supplied to the indicated output stream */
         fputs(recvline, stdout);
     }
 }
@@ -57,24 +62,31 @@ int main(int argc, char **argv)
 {
     struct sockaddr_in servaddr;;
 
-    /*Must include an IP address of this will flag*/
+    /* must include an ip address or this will flag */
     if(argc != 2){
         printf("Usage: tcpClient <IPaddress>\n");
         exit(0);
     }
 
-    /*Create a stream socket using TCP,Internet Protocal IPv4,
-     *full-duplex stream */
+    /* create a stream socket using tcp,internet protocal IPv4,
+     * full-duplex stream */
     int sockfd = socket(AF_INET, SOCK_STREAM, 0); 
 
+    /* places n zero-valued bytes in the address servaddr */
     bzero(&servaddr, sizeof(servaddr));
+
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(SERV_PORT);
+
+    /* converts IPv4 addresses from text to binary form */
     inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
 
+    /* attempts to make a connection on a socket */
     connect(sockfd, (SA *) &servaddr, sizeof(servaddr));
 
-    str_Client(stdin, sockfd);
+    /* takes inputting string and outputs it to the server */
+    SendReceive(stdin, sockfd);
 
+    /* end client */
     exit(0);
 }
