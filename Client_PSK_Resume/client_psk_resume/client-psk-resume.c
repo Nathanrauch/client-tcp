08e6b2e04fdd1674ba32c58f91c21cec3c26cdf2
Nathanrauch <nathan@wolfssl.com>
2014-06-10 12:19:51 -0600

    Update client-psk-resume.c

--- Client_PSK_Resume/client_psk_resume/client-psk-resume.c
+++ Client_PSK_Resume/client_psk_resume/client-psk-resume.c
@@ -29,9 +29,8 @@
 #include    <unistd.h>
 #include    <cyassl/ssl.h>  /* must include this to use cyassl security */
 
-#define    MAXLINE 256      /* max text line length */
-#define    SERV_PORT 11111  /* default port*/
-#define    SA  struct sockaddr
+#define     MAXLINE 256      /* max text line length */
+#define     SERV_PORT 11111  /* default port*/
 
 /*
  *psk client set up.
@@ -59,7 +58,7 @@ static inline unsigned int My_Psk_Client_Cb(CYASSL* ssl, const char* hint,
 /*
  * this function will send the inputted string to the server
  */ 
-void SendReceive(FILE *fp, CYASSL* ssl){
+void SendReceive(CYASSL* ssl){
     char sendline[MAXLINE]; /* string to send to the server */
     
         fgets(sendline, MAXLINE, fp);
@@ -69,65 +68,70 @@ void SendReceive(FILE *fp, CYASSL* ssl){
 
 int main(int argc, char **argv){
     
+    int sockfd, sock, ret;
     CYASSL* ssl;
     CYASSL*         sslResume = 0;
     CYASSL_SESSION* session   = 0;
+    CYASSL_CTX* ctx;
     struct sockaddr_in servaddr;;
 
     /* must include an ip address of this will flag */
     if (argc != 2) {
         printf("Usage: tcpClient <IPaddress>\n");
-        exit(0);
+        return 1;
     }
     
     CyaSSL_Init();  /* initialize cyaSSL */
-    CYASSL_CTX* ctx;
-            
+    
     /* create and initialize CYASSL_CTX structure */
     if ((ctx = CyaSSL_CTX_new(CyaTLSv1_2_client_method())) == NULL) {
         fprintf(stderr, "SSL_CTX_new error.\n");
-        exit(EXIT_FAILURE);
-       }
+       return 1;
+    }
                 
      /* load ca certificates into CYASSL_CTX.
       * these will be used to verify the server we connect to */
-     if (CyaSSL_CTX_load_verify_locations(ctx,"../certs/ca-cert.pem",0) != 
+    if (CyaSSL_CTX_load_verify_locations(ctx,"../certs/ca-cert.pem",0) != 
             SSL_SUCCESS) {
          fprintf(stderr, "Error loading ../certs/ca-cert.pem, "
                  "please check the file.\n");
-         exit(EXIT_FAILURE);
-      }
+         return 1;
+     }
 
     /* create a stream socket using tcp,internet protocal IPv4,
      * full-duplex stream */
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
     
     /* places n zero-valued bytes in the address servaddr */
-    bzero(&servaddr, sizeof(servaddr));
+    memset(&servaddr, sizeof(servaddr));
 
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(SERV_PORT);
 
     /* converts IPv4 addresses from text to binary form */
-    inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+    ret = inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+    
+    if (ret != 1){
+        return 1;
+    }
     
     /* set up pre shared keys */
     CyaSSL_CTX_set_psk_client_callback(ctx,My_Psk_Client_Cb);
 
     /* attempts to make a connection on a socket */
-    connect(sockfd, (SA *) &servaddr, sizeof(servaddr));
+    connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
     
     /* creat cyassl object after each tcp connct */
     if ( (ssl = CyaSSL_new(ctx)) == NULL) {
         fprintf(stderr, "CyaSSL_new error.\n");
-        exit(EXIT_FAILURE);
+        return 1;
     }
 
     /* associate the file descriptor with the session */
     CyaSSL_set_fd(ssl, sockfd);
 
      /* takes inputting string and outputs it to the server */
-    SendReceive(stdin, ssl);
+    SendReceive(ssl);
 
     /* Save the session ID to reuse */
     session   = CyaSSL_get_session(ssl);
@@ -149,10 +153,10 @@ int main(int argc, char **argv){
      */
 
     /* start a new socket connection */
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    sock = socket(AF_INET, SOCK_STREAM, 0);
     
     /* connect to the socket */
-    connect(sock, (SA *) &servaddr, sizeof(servaddr));
+    connect(sock, (struct sockaddr *) &servaddr, sizeof(servaddr));
 
     /* set the session ID to connect to the server */
     CyaSSL_set_fd(sslResume, sock);
@@ -165,7 +169,7 @@ int main(int argc, char **argv){
     }
 
     /* takes inputting string and outputs it to the server */
-    SendReceive(stdin, sslResume);
+    SendReceive(sslResume);
 
     /* check to see if the session id is being reused */
     if (CyaSSL_session_reused(sslResume))
@@ -185,5 +189,5 @@ int main(int argc, char **argv){
     CyaSSL_Cleanup();
     
     /* exit client */
-    exit(0);
+    return ret;
 }
