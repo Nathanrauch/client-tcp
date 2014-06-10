d0974740f7c408d2c0a3f85e641422ed8a5c5ef3
Nathanrauch <nathan@wolfssl.com>
2014-06-10 11:51:44 -0600

    Update client-tcp.c

--- Client-TCP/client-tcp.c
+++ Client-TCP/client-tcp.c
@@ -32,17 +32,17 @@
 
 #define    MAXLINE     256    /* max text line length */
 #define    SERV_PORT   11111
-#define    SA  struct sockaddr
 
 /*
  * this function will send the inputted string to the server and then 
  * recieve the string from the server outputing it to the termial
  */ 
-void SendReceive(FILE *fp, int sockfd){
+int SendReceive(int sockfd)
+{
     char sendline[MAXLINE]; /* string to send to the server */
     char recvline[MAXLINE]; /* string received from the server */
 
-    while (fgets(sendline, MAXLINE, fp) != NULL) {
+    while (fgets(sendline, MAXLINE, stdin) != NULL) {
        
         /* write string to the server */
         write(sockfd, sendline, strlen(sendline));
@@ -50,7 +50,7 @@ void SendReceive(FILE *fp, int sockfd){
         /* flags if the server stopped before the client could end */     
         if (read(sockfd, recvline, MAXLINE) == 0) {
             printf("Client: Server Terminated Prematurely!\n");
-            exit(0);
+            return 1;
         }
 
         /* writes the string supplied to the indicated output stream */
@@ -58,37 +58,38 @@ void SendReceive(FILE *fp, int sockfd){
     }
 }
 
-int main(int argc, char **argv){
-    
-    struct sockaddr_in servaddr;;
+int main(int argc, char **argv)
+{
+    int sockfd, ret; 
+    struct sockaddr_in servaddr;
 
     /* must include an ip address or this will flag */
     if (argc != 2) {
         printf("Usage: tcpClient <IPaddress>\n");
-        exit(0);
+        return 1;
     }
 
     /* create a stream socket using tcp,internet protocal IPv4,
      * full-duplex stream */
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
 
     /* places n zero-valued bytes in the address servaddr */
     bzero(&servaddr, sizeof(servaddr));
 
     servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(SERV_PORT); 
+    servaddr.sin_port   = htons(SERV_PORT); 
 
     /* converts IPv4 addresses from text to binary form */
     inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
 
     /* attempts to make a connection on a socket */
-    connect(sockfd, (SA *) &servaddr, sizeof(servaddr));
+    connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
 
     /* takes inputting string and outputs it to the server */
-    SendReceive(stdin, sockfd);
+    ret = SendReceive(sockfd);
 
     /* close socket and connection */
     close(sockfd);
-    /* end client */
-    exit(0);
+    
+    return ret;
 }
